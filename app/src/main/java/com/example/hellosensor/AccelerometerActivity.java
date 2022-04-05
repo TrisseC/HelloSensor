189f47bbcad78c303d85d730d03a5bb041619f48
Trisse <tr8531pe-s@student.lu.se>
2022-04-05 15:35:39 +0200

    allt

---
+++ app/src/main/java/com/example/hellosensor/AccelerometerActivity.java
@@ -0,0 +1,111 @@
+package com.example.hellosensor;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.widget.TextView;
+
+public class AccelerometerActivity extends Activity implements SensorEventListener {
+
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+
+    private TextView currentX, currentY, currentZ;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accelerometer);
+        initializeViews();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            // success! we have an accelerometer
+
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            // fai! we dont have an accelerometer!
+        }
+
+
+    }
+
+    public void initializeViews() {
+        currentX = (TextView) findViewById(R.id.currentX);
+        currentY = (TextView) findViewById(R.id.currentY);
+        currentZ = (TextView) findViewById(R.id.currentZ);
+    }
+
+    //onResume() register the accelerometer for listening the events
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    //onPause() unregister the accelerometer for stop listening the events
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // clean current values
+        displayCleanValues();
+        // display the current x,y,z accelerometer values
+        displayCurrentValues();
+
+
+        deltaX = Math.round(event.values[0]*10.0f)/10.0f;
+        deltaY = Math.round(event.values[1]*10.0f)/10.0f;
+        deltaZ = Math.round(event.values[2]*10.0f)/10.0f;
+
+        dothething();
+
+    }
+
+    private void dothething() {
+        TextView thingText = (TextView) findViewById(R.id.thingText);
+        if(deltaX > deltaY){
+            thingText.setText("lutar åt vänster");
+        }
+        else if(-deltaX > deltaY){
+            thingText.setText("lutar åt höger");
+        }
+        else{
+            thingText.setText("lutar inte så mycket");
+        }
+    }
+
+    public void displayCleanValues() {
+        currentX.setText("0.0");
+        currentY.setText("0.0");
+        currentZ.setText("0.0");
+    }
+
+    // display the current x,y,z accelerometer values
+    public void displayCurrentValues() {
+        currentX.setText(Float.toString(deltaX));
+        currentY.setText(Float.toString(deltaY));
+        currentZ.setText(Float.toString(deltaZ));
+    }
+
+}
